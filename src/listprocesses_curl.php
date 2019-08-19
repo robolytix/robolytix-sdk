3060a8a0724e814efbc099e5615ae569eda4d645
ofiala <ofiala@mujmail.cz>
2019-08-20 00:45:30 +0200

    adding get call

---
+++ src/listprocesses_curl.php
@@ -0,0 +1,27 @@
+<?php
+
+    // Set API KEY generated in your Account settings 
+    $API_KEY = 'XXX';
+     
+    // Prepare new cURL resource
+    $ch = curl_init('https://api.robolytix.com/v1/listprocesses');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+    curl_setopt($ch, CURLOPT_POST, false);    
+     
+    // Set HTTP Header for POST request 
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($payload),
+        'Authorization: ' . $API_KEY)
+    );
+     
+    // Submit the POST request
+    $result = curl_exec($ch);
+     
+    // Close cURL session handle
+    curl_close($ch);
+ 
+    // Print response
+    var_dump($result);
+?>
